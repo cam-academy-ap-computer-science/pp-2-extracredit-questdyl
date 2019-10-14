6f1d196318828288039721e1465856f6a3368bc0
questdyl000 <questdyl000@CAM-LAB-29.bgsd.k12.wa.us>
2019-10-14 07:50:37 -0700

    Major WIP

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -1,8 +1,34 @@
+/*  Pseudo-code
+ * needle
+ * 		print "        ||" 4 times
+ * top
+ * 		print 4 times
+ * 			print spaces according to line
+ * 			print "__/"
+ * 			print ::: according to line
+ * 			print ||
+ * 			:::
+ * 			\__
+ *		print |""""""""""""""""""""""""|
+ *		print 4 times
+ *			print spaces according to line
+ *			print \_/
+ *			print \/ according to line
+ *			print \_/
+ *		
+ */
+
 
 public class ExtraCredit {
 
-	public static void main(String[] args) {
-
+	public static void needle() {
+		for (int i = 1; i <= 4; i++) {
+			System.out.println("            ||");
+		}
 	}
+	
+	public static void top
+	
 
+	
 }
